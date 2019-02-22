4356fee1d1fc8e78b3800e09337c5dba058f49c9
Chris Ducote <chrisd@slappy.local>
2019-02-22 11:02:47 -0500

    super late initial commit

---
+++ notes-app-client/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
+import Routes from './Routes'
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+    render() {
+      const childProps = {
+        isAuthenticated: this.state.isAuthenticated,
+        userHasAuthenticated: this.userHasAuthenticated
+      };
+
+      return (
+        <div className="App container">
+          <Navbar fluid collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">Scratch</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav pullRight>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Routes childProps={childProps} />
+        </div>
+      );
+    }
+}
+
+export default App;
---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "slappy-notes-app-upload"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://giz0i4zzs3.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_bunZGQtjC",
+    APP_CLIENT_ID: "6lhml3k7spgkf97vnp811k7lnl",
+    IDENTITY_POOL_ID: "us-east-2:dfce42cf-4a34-4b2b-92a5-4856a4b2f18a"
+  }
+};
