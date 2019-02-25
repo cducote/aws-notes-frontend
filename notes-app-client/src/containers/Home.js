5843508c8c5c542dd0392b8b359f339f219b838e
Chris Ducote <chrisd@slappy.local>
2019-02-25 11:25:33 -0500

    show one note route created

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup } from "react-bootstrap";
 import "./Home.css";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { API } from 'aws-amplify'
 
 export default class Home extends Component {
@@ -33,7 +34,28 @@ export default class Home extends Component {
   }
 
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map(
+      (note, i) =>
+        i !== 0
+          ? <LinkContainer
+              key={note.noteId}
+              to={`/notes/${note.noteId}`}
+            >
+              <ListGroupItem header={note.content.trim().split("\n")[0]}>
+                {"Created: " + new Date(note.createdAt).toLocaleString()}
+              </ListGroupItem>
+            </LinkContainer>
+          : <LinkContainer
+              key="new"
+              to="/notes/new"
+            >
+              <ListGroupItem>
+                <h4>
+                  <b>{"\uFF0B"}</b> Create a new note
+                </h4>
+              </ListGroupItem>
+            </LinkContainer>
+    );
   }
 
   renderLander() {
